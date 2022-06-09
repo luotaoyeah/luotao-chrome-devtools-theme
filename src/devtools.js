31248d5cd56859ba0cc5f400db1795920b143708
luotao <luotaoyeah@gmail.com>
2022-06-09 22:44:37 +0800

    feat(1.8): .payload-name

--- src/devtools.js
+++ src/devtools.js
@@ -1,8 +1,8 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
-    chrome.devtools.panels.applyStyleSheet(xhr.responseText);
-  }
+    if (this.readyState === 4 && this.status === 200) {
+        chrome.devtools.panels.applyStyleSheet(xhr.responseText);
+    }
 };
 xhr.open('GET', '/styles.css');
 xhr.send();
